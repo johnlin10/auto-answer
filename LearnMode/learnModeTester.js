e3fc12b2d4478cf8cdf7b1d2382607764ca78f17
Johnolin10 <johno1001741060168@gmail.com>
2024-03-21 13:45:00 +0800

    Add scripts for LearnMode test automation
    Introduced two new scripts to enhance the user experience on learnmode.net. The first script enables rapid random answer selection during chapter tests, effectively answering about 20 questions per second, facilitating quick progress through materials. The second script circumvents time-based access restrictions set by instructors, allowing users to enter tests anytime, mitigating issues with the "test ended" barrier. Both scripts are executed via the browser's console.
    
    Note: Ethical considerations of automating responses and bypassing restrictions should be addressed to ensure fair use.

---
+++ LearnMode/learnModeTester.js
@@ -0,0 +1,31 @@
+/**
+ * 用途：快速隨機作答 (大約20題/秒)
+ * 使用範圍：learnmode.net 課程中的章節試題
+ * 使用方法：複製整段程式，點選題目到第一題，在控制台底部輸入框貼上，Enter 後開始運行
+ */
+/**
+ * 如要停止此程式，請運行以下程式：
+ * clearInterval(learnModeTester);
+ */
+var learnModeTester = setInterval(function () {
+  // 找到選項列表內的選項
+  const elements = document.querySelectorAll(
+    'ul[data-v-204f78ad] > span > label'
+  )
+
+  // 隨機選擇選項
+  const randomIndex = Math.floor(Math.random() * elements.length)
+  const selectedElement = elements[randomIndex]
+
+  // 點擊隨機選項
+  selectedElement.click()
+
+  // 等待一小段時間，點擊下一題
+  setTimeout(() => {
+    const buttonSection = document.querySelector('div.button-section')
+    const buttons = buttonSection.querySelectorAll('button')
+
+    // 下一題是第二個按鈕，最後一題時會變成繳交
+    buttons[1].click()
+  }, 25)
+}, 50)
---
+++ LearnMode/unlockTest.js
@@ -0,0 +1,11 @@
+/**
+ * 用途：解除作答時段限制（老師可能限制某一時間段才能進入測試）
+ * 使用範圍：進入 learnmode.net 課程中的章節選單介面後，看到 “測驗已結束” 就可以使用運行程式來解除
+ * 使用方法：複製整段程式，在瀏覽器控制台底部輸入框中貼上，Enter 解除限制
+ */
+// 尋找按鈕
+const unlockTest = document.querySelector('button[data-v-3755d284]')
+if (unlockTest) {
+  // 解除按鈕禁用
+  unlockTest.disabled = false
+}
